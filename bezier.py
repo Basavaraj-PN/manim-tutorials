276b95c841c9a320f14f8b851779e2cb2549cc55
Basavaraj-PN <bnarasapur@gmail.com>
2023-09-19 14:05:31 +0530

    *Code cleaning

--- bezier.py
+++ bezier.py
@@ -10,11 +10,11 @@ MAROON_A: str = "#ECABC1"
 
 color_array = [PURE_RED, PURE_GREEN, PURE_BLUE, MAROON_A]
 
-A = np.array((-2., -2., 0.))
+A = np.array((-2., -8., 0.))
 B = np.array((1., 2., 0.))
 C = np.array((5., -5., 0.))
 D = np.array((10, 2., 0.))
-E = np.array((18, -2., 0.))
+E = np.array((18, -8., 0.))
 
 points = [A, B, C, D, E]
 labels = ["A", "B", "C", "D", "E"]
@@ -46,84 +46,104 @@ class Bezier(Scene):
         self.camera.frame.move_to(6 * RIGHT + 2 * DOWN)
         self.camera.frame.set_z(8)
 
-        self.camera.background_color = BLUE
-
         lines = make_lines()
-        fixed_dots = [Dot(radius=0.175).set_color(WHITE) for _ in range(len(points))]
+        fixed_dots = [Dot(radius=0.175, point=points[_]).set_color(ORANGE) for _ in range(len(points))]
         points_label_txt = [Text(labels[i]) for i in range(len(labels))]
         num_of_moving_dots = sum(len(lines) - i for i in range(len(lines)))
         moving_dots = [Dot(radius=0.15) for i in range(num_of_moving_dots)]
-
-        welocome_text = Text("Beauty of Bézier curve", gradient=(PURE_RED, PURE_GREEN, PURE_BLUE, MAROON_A)).scale(3)
-        welocome_text.next_to(LEFT_SIDE + 4 * RIGHT)
-        self.play(Write(welocome_text))
-        self.wait()
-        self.play(FadeOut(welocome_text))
+        dots = fixed_dots + moving_dots
+        fixed_plus_moving_dots = []
+        for dot in dots:
+            fixed_plus_moving_dots.append(dot)
 
         self.play(
-            *[Write(lines[i].set_stroke(BLACK, 10)) for i in range(len(lines))]
-        )
-        self.play(
-            *[Write(fixed_dots[i].move_to(lines[i].get_start())) for i in range(len(lines))]
-        )
-        self.play(
-            Write(Dot(radius=0.175).set_color(WHITE).move_to(lines[-1].get_end()))
-        )
-        self.play(
-            *[Write(points_label_txt[i].next_to(lines[i].get_start(), LEFT)) for i in range(len(lines))]
-        )
-        self.play(
-            Write(Text(labels[-1]).next_to(lines[-1].get_end(), LEFT))
+            *[Write(fixed_plus_moving_dots[i]) for i in range(len(fixed_plus_moving_dots))]
         )
 
-        # TODO: Comments
+        self.play(
+            *[Write(lines[i].set_stroke(WHITE, 8)) for i in range(len(lines))]
+        )
+
+        # # TODO: Comments
         n = 0
         for i in range(len(lines)):
             lerps = lerps_at_step(len(lines), i)
+            # print(lerps)
             for j in range(lerps):
                 moving_dots[n + j].set_color(color_array[i])
             n = n + lerps
 
-        # TODO: where to start moving dots initially?
-        self.play(
-            *[Write(moving_dots[i].move_to(LEFT * 100)) for i in range(num_of_moving_dots)]
-        )
 
-        # Lerp
-        moving_dots[0].add_updater(lambda obj: self.updater_wrapper(obj, A, B))
-        moving_dots[1].add_updater(lambda obj: self.updater_wrapper(obj, B, C))
-        moving_dots[2].add_updater(lambda obj: self.updater_wrapper(obj, C, D))
-        moving_dots[3].add_updater(lambda obj: self.updater_wrapper(obj, D, E))
+        # for i in range(len(lines)):
+        #     lerps = lerps_at_step(len(lines), i)
+        #     cnt =0
+        #     for j in range(len(fixed_dots), len(fixed_plus_moving_dots)):
+        #         # print(j)
+        #         # if(cnt < lerps):
+        #         print(f'pairs={j, j-len(fixed_dots), j-len(fixed_dots)+1}')
+        #         cnt =  cnt + 1
+        #     print("***")
 
-        moving_dots[4].add_updater(
-            lambda obj: self.updater_wrapper(obj, moving_dots[0].get_center(), moving_dots[1].get_center()))
-        moving_dots[5].add_updater(
-            lambda obj: self.updater_wrapper(obj, moving_dots[1].get_center(), moving_dots[2].get_center()))
-        moving_dots[6].add_updater(
-            lambda obj: self.updater_wrapper(obj, moving_dots[2].get_center(), moving_dots[3].get_center()))
 
-        moving_dots[7].add_updater(
-            lambda obj: self.updater_wrapper(obj, moving_dots[4].get_center(), moving_dots[5].get_center()))
-        moving_dots[8].add_updater(
-            lambda obj: self.updater_wrapper(obj, moving_dots[5].get_center(), moving_dots[6].get_center()))
-        moving_dots[9].add_updater(
-            lambda obj: self.updater_wrapper(obj, moving_dots[7].get_center(), moving_dots[8].get_center()))
+        # self.play(Write(Text("Done").next_to(ORIGIN)))
+        # d = d + 1
 
-        # path = [TracedPath(moving_dots[i].get_center).set_stroke(color=PINK) for i in range(len(moving_dots))]
-        # self.add(*path)
+        # 5 = 0,1
+        # 6 = 1,2
+        # 7 = 2,3
+        # 8 = 3,4
 
-        n = 0
-        for i in range(len(lines)):
-            lerps = lerps_at_step(len(lines), i)
-            for j in range(lerps):
-                moving_dots[n + j].set_color(color_array[i])
-                path = TracedPath(moving_dots[n + j].get_center).set_stroke(color=color_array[i], width=5)
-                self.add(path)
-            n = n + lerps
+        # 9 = 5,6
+        # 10 = 6, 7
+        # 11 = 7, 8
 
-        dark_path = TracedPath(moving_dots[-1].get_center).set_stroke(color=PINK, width=13)
-        self.add(dark_path)
-        # self.play(FadeOut(dark_path))
+        # 12 = 9, 10
+        # 13 = 10, 11
 
-        self.play(self.t.animate.set_value(1), rate_func=there_and_back, run_time=15)
+        # 14 = 12, 13
+
+        # loop start = 5 loop_end=14
+        fixed_dots = fixed_plus_moving_dots[:5]
+        moving_dots_ = fixed_plus_moving_dots[5:]
+
+        moving_dots_[0].add_updater(
+            lambda obj: self.updater_wrapper(obj, fixed_dots[0].get_center(),
+                                             fixed_dots[1].get_center()))
+        moving_dots_[1].add_updater(
+            lambda obj: self.updater_wrapper(obj, fixed_dots[1].get_center(),
+                                             fixed_dots[2].get_center()))
+        moving_dots_[2].add_updater(
+            lambda obj: self.updater_wrapper(obj, fixed_dots[2].get_center(),
+                                             fixed_dots[3].get_center()))
+        moving_dots_[3].add_updater(
+            lambda obj: self.updater_wrapper(obj, fixed_dots[3].get_center(),
+                                             fixed_dots[4].get_center()))
+
+        #
+        moving_dots_[4].add_updater(
+            lambda obj: self.updater_wrapper(obj, moving_dots_[0].get_center(),
+                                             moving_dots_[1].get_center()))
+        moving_dots_[5].add_updater(
+            lambda obj: self.updater_wrapper(obj, moving_dots_[1].get_center(),
+                                             moving_dots_[2].get_center()))
+        moving_dots_[6].add_updater(
+            lambda obj: self.updater_wrapper(obj, moving_dots_[2].get_center(),
+                                             moving_dots_[3].get_center()))
+
+        moving_dots_[7].add_updater(
+            lambda obj: self.updater_wrapper(obj, moving_dots_[4].get_center(),
+                                             moving_dots_[5].get_center()))
+        moving_dots_[8].add_updater(
+            lambda obj: self.updater_wrapper(obj, moving_dots_[5].get_center(),
+                                             moving_dots_[6].get_center()))
+        moving_dots_[9].add_updater(
+            lambda obj: self.updater_wrapper(obj, moving_dots_[7].get_center(),
+                                             moving_dots_[8].get_center()))
+
+        # Draw path
+        dark_path1 = TracedPath(fixed_plus_moving_dots[-1].get_center).set_stroke(color=GREEN, width=13)
+        self.add(dark_path1)
+
+
+        self.play(self.t.animate.set_value(1), rate_func=there_and_back, run_time=5)
         # self.wait()
